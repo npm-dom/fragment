a88064c6b7ea1187719079c20dd104c3491cb371
Raynos <raynos2@gmail.com>
2012-08-20 20:47:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ module.exports = Fragment
 
 function Fragment(html) {
     var div = document.createElement("div"),
-        args = arguments,
-        i = 1,
         fragment = document.createDocumentFragment()
 
-    div.innerHTML = html.replace(/%s/g, function(){
-        return String(args[i++])
-    })
+    div.innerHTML = html
 
     while (div.hasChildNodes()) {
         fragment.appendChild(div.firstChild)
